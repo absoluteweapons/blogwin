b76a17b3ac70742aba0a8a4fbd962f3f631e62aa
Jamie Evawin <iamjevawin@gmail.com>
2021-07-25 13:09:21 +0100

    fix jsmin prod

--- _src/_config/jsmin.js
+++ _src/_config/jsmin.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 module.exports = async (code, callback) => {
   try {
     // minify for production
-    const minified = (process.env.NODE_ENV !== "production") ? await minify(code) : code;
-    callback(null, minified.code);
+    if (process.env.NODE_ENV === "production") {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } else {
+      callback(null, code);
+    }
   } catch (err) {
     console.error("Terser error: ", err);
     // Fail gracefully.
